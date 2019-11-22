3ca5a5e69757fa261d9a54eed4f930a9239a1e41
m3ales <mattfdavies12@gmail.com>
2019-11-23 01:15:22 +0200

    got rid of uneeded func_ace macro

--- MIRA_ACE_Medical_Notification/config_macros.hpp
+++ MIRA_ACE_Medical_Notification/config_macros.hpp
@@ -1,7 +1,6 @@
 #define ADDON MIRA_ACE_Medical_Notification
 #define ADDON_NAME Ace Medical Menu Notification
 #define FUNC(name) ADDON##_fnc_##name
-#define FUNC_ACE(module,name) ace_##module##_fnc_##name
 #define QUOTE(target) #target
 #define CONCAT(a,b) a##b
 #define COMPILE_FILE(name) compile preprocessFileLineNumbers 'ADDON\##name##.sqf'
